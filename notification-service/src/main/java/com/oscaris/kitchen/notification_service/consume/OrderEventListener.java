5caf29df41c0a2cca86d00c2cc5306a62b91f92d
ameda-kevin <amedakevin@gmail.com>
2024-09-28 16:55:41 +0300

    updates on the consumers end with fetch attempts, min & max delay times

--- notification-service/src/main/java/com/oscaris/kitchen/notification_service/consume/OrderEventListener.java
+++ notification-service/src/main/java/com/oscaris/kitchen/notification_service/consume/OrderEventListener.java
@@ -8,6 +8,8 @@ package com.oscaris.kitchen.notification_service.consume;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 
@@ -18,7 +20,16 @@ public class OrderEventListener {
     private String topicName;
 
     @KafkaListener(id = "orderEventListener",topics = "my-topic" )
+     // 😅when to retry n when delay updated 😅
+    //😅attempts set to 3 even though the maxDelay is not adhered but atleast it tried for😅
+    //😅 3 times then the error can be thrown. 😅
+    @RetryableTopic(attempts = "3", backoff = @Backoff(delay = 2000L, maxDelay = 10000L,
+    multiplier = 2))
     public void consume(String order){
         System.out.println("Received event :: "+order);
+        throw new RuntimeException();
+        /*
+        * just above code added as a way of testing the @Retryables
+        * */
     }
 }
