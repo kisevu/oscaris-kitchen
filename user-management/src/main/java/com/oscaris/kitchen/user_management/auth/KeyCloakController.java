7879649e1f9a51d67669b6658eeb6ae92b273f5a
ameda-kevin <amedakevin@gmail.com>
2024-10-07 11:44:15 +0300

    updated the security and now things are working correctly and endpoints are being authenticated successfully

--- user-management/src/main/java/com/oscaris/kitchen/user_management/auth/KeyCloakController.java
+++ user-management/src/main/java/com/oscaris/kitchen/user_management/auth/KeyCloakController.java
@@ -7,6 +7,7 @@ package com.oscaris.kitchen.user_management.auth;
 */
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class KeyCloakController {
 
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<String> getAdminDetails(){
         return ResponseEntity.ok("Hello Admin");
     }
 
     @GetMapping("/user")
+    @PreAuthorize("hasRole('user')")
     public ResponseEntity<String> getUserDetails(){
         return ResponseEntity.ok("Hello User");
     }
