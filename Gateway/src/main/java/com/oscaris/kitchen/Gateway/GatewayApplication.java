70cf952158f28b0a8fc727691f1384a30c6a7e07
ameda-kevin <amedakevin@gmail.com>
2024-10-28 01:26:04 +0300

    containerized microservices and deployed to dockerhub awaiting orche with k8s

--- Gateway/src/main/java/com/oscaris/kitchen/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/oscaris/kitchen/Gateway/GatewayApplication.java
@@ -1,13 +1,17 @@
 package com.oscaris.kitchen.Gateway;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+		Dotenv dotenv = Dotenv.configure().load();
+
 	}
 
 }
